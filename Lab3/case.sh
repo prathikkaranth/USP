1f954dcd3653af12c72b1ac91149a3394c7af10f
prathikkaranth <53780002+prathikkaranth@users.noreply.github.com>
2020-10-13 15:34:33 +0530

    Add files via upload

---
+++ Lab3/case.sh
@@ -0,0 +1,20 @@
+echo "Enter arithmetic operation: "
+read a op b
+
+case $op in
+    "+") res=`expr $a + $b`
+    ;;
+    "-") res=`expr $a - $b`
+    ;;
+    "*") res=`expr $a \* $b`
+    ;;
+    "/") res=`expr $a / $b`
+    ;;
+    "%") res=`expr $a % $b`
+    ;;
+    *) echo "Invalid operator."
+    exit 1
+    ;;
+esac
+
+echo "$a $op $b = $res"
